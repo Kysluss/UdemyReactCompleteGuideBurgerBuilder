ac2199bfc9c89091b13c46386db1fc6576e565b4
unknown <Kysluss@aol.com>
2018-08-18 12:30:07 -0400

    Fixed error with no validation set up

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,7 @@ class ContactData extends Component {
                     ]
                 }, 
                 value: '', 
+                validation: {}, 
                 valid: true
             }
         }, 
@@ -123,6 +124,11 @@ class ContactData extends Component {
 
     checkValidity(value, rules) {
         let isValid = true;
+
+        // If no rules are set up, short circuit
+        if(!rules) {
+            return true;
+        }
 
         if(rules.required) {
             isValid = value.trim() !== '' && isValid;
