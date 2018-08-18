9b4101780e62c3161513a4617afb928f4417beb2
Kyle Slusser <SLUKY@intlogistics.com>
2018-08-18 08:30:26 -0400

    Created form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
+import Button from '../../../components/UI/Button/Button';
+
 class ContactData extends Component {
     state = {
-    
+        name: '', 
+        email: '', 
+        address: {
+            street: '', 
+            postalCode: ''
+        }
     }
     
     render() {
         return (
-            <div>Content</div>
+            <div>
+                <h4>Enter Your Contact Data</h4>
+                <form>
+                    <input type="text" name="name" placeholder="Your name" />
+                    <input type="text" name="email" placeholder="Your EMail" />
+                    <input type="text" name="street" placeholder="Your Street" />
+                    <input type="text" name="postal" placeholder="Your Postal Code" />
+                    <Button btonType="Success">ORDER</Button>
+                </form>
+            </div>
         );
     }
 }
