e8ff411fb870ef7695352c7e2c953bd91bf3800d
unknown <Kysluss@aol.com>
2018-08-18 12:24:19 -0400

    Added form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,7 +16,12 @@ class ContactData extends Component {
                     type: 'text', 
                     placeholder: 'Your name'
                 }, 
-                value: ''
+                value: '', 
+                validation: {
+                    required: true
+                }, 
+                valid: false, 
+                touched: false
             }, 
             street: {
                 elementType: 'input', 
@@ -24,7 +29,12 @@ class ContactData extends Component {
                     type: 'text', 
                     placeholder: 'Street'
                 }, 
-                value: ''
+                value: '', 
+                validation: {
+                    required: true
+                }, 
+                valid: false, 
+                touched: false
             }, 
             zipCode: {
                 elementType: 'input', 
@@ -32,7 +42,14 @@ class ContactData extends Component {
                     type: 'text', 
                     placeholder: 'ZIP Code'
                 }, 
-                value: ''
+                value: '', 
+                validation: {
+                    required: true, 
+                    minLength: 5, 
+                    maxLength: 5
+                }, 
+                valid: false, 
+                touched: false
             }, 
             country: {
                 elementType: 'input', 
@@ -40,7 +57,12 @@ class ContactData extends Component {
                     type: 'text', 
                     placeholder: 'Country'
                 }, 
-                value: ''
+                value: '', 
+                validation: {
+                    required: true
+                }, 
+                valid: false, 
+                touched: false
             }, 
             email: {
                 elementType: 'email', 
@@ -48,7 +70,12 @@ class ContactData extends Component {
                     type: 'text', 
                     placeholder: 'Your E-Mail'
                 }, 
-                value: ''
+                value: '', 
+                validation: {
+                    required: true
+                }, 
+                valid: false, 
+                touched: false
             }, 
             deliveryMethod: {
                 elementType: 'select', 
@@ -61,7 +88,8 @@ class ContactData extends Component {
                 value: ''
             }
         }, 
-        loading: false 
+        loading: false, 
+        formIsValid: false
     }
 
     orderHandler = (event) => {
@@ -69,10 +97,16 @@ class ContactData extends Component {
 
         this.setState({ loading: true });
 
+        const formData = {};
+
+        for(let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+
         const order = {
             ingredients: this.props.ingredients, 
             price: this.props.totalPrice, 
-            deliveryMethod: 'fastest'
+            orderData: formData
         }
         // The .json extension is firebase specific
         // We need it for firebase to understand our request
@@ -86,6 +120,24 @@ class ContactData extends Component {
             });
     }
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if(rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         // {...this.state.orderForm} does not perform a deep clone
         const updatedOrderForm = {...this.state.orderForm};
@@ -96,11 +148,20 @@ class ContactData extends Component {
         const updatedFormElement = {...updatedOrderForm[inputIdentifier]};
 
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
 
         updatedOrderForm[inputIdentifier] = updatedFormElement;
 
+        let formIsValid = true;
+
+        for(let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+
         this.setState({
-            orderForm: updatedOrderForm
+            orderForm: updatedOrderForm, 
+            formIsValid: formIsValid
         });
     }
     
@@ -115,17 +176,19 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
-                
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input 
                         key={formElement.id}
                         elementType={formElement.config.elementType} 
                         elementConfig={formElement.config.elementConfig} 
                         value={formElement.config.value} 
+                        invalid={!formElement.config.valid} 
+                        shouldValidate={formElement.config.validation} 
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         );
 
