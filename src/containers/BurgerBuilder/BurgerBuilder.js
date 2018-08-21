5ba7f53c7994e14611917dd14e77c55144f803b0
unknown <Kysluss@aol.com>
2018-08-21 14:58:29 -0400

    Code cleanup

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,32 +11,13 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 import * as actionTypes from '../../store/actions';
 
-// const INGREDIENT_PRICES = {
-//     salad: 0.5, 
-//     cheese: 0.4, 
-//     meat: 1.3, 
-//     bacon: 0.7 
-// };
-
 class BurgerBuilder extends Component {
     state = {
-        // ingredients: null, 
-        // totalPrice: 4, 
         purchasable: false, 
         purchasing: false, 
         loading: false, 
         error: false
     }
-
-    // componentDidMount() {
-    //     axios.get('https://react-my-burger-9263a.firebaseio.com/ingredients.json')
-    //         .then(response => {
-    //             this.setState({ingredients: response.data});
-    //         })
-    //         .catch(err => {
-    //             this.setState({error: true});
-    //         });
-    // }
 
     updatePurchaseState(ingredients) {
         const sum = Object.keys(ingredients)
@@ -51,53 +32,6 @@ class BurgerBuilder extends Component {
 
         this.setState({ purchasable: sum > 0 });
     }
-
-    // addIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type];
-    //     const updatedCount = oldCount + 1;
-
-    //     const updatedIngredients = {
-    //         ...this.state.ingredients 
-    //     };
-
-    //     updatedIngredients[type] = updatedCount;
-
-    //     const priceAddition = INGREDIENT_PRICES[type];
-    //     const oldPrice = this.props.price;
-    //     const newPrice = oldPrice + priceAddition;
-
-    //     this.setState({
-    //         ingredients: updatedIngredients, 
-    //         totalPrice: newPrice
-    //     });
-    //     this.updatePurchaseState(updatedIngredients);
-    // }
-
-    // removeIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type];
-
-    //     if(oldCount <= 0) {
-    //         return;
-    //     }
-
-    //     const updatedCount = oldCount - 1;
-
-    //     const updatedIngredients = {
-    //         ...this.state.ingredients 
-    //     };
-
-    //     updatedIngredients[type] = updatedCount;
-
-    //     const priceDeduction = INGREDIENT_PRICES[type];
-    //     const oldPrice = this.props.price;
-    //     const newPrice = oldPrice - priceDeduction;
-
-    //     this.setState({
-    //         ingredients: updatedIngredients, 
-    //         totalPrice: newPrice
-    //     });
-    //     this.updatePurchaseState(updatedIngredients);
-    // }
 
     purchaseHandler = () => {
         this.setState({
