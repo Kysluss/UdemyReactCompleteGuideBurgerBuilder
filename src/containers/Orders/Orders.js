ca159d34b5d237df981618513e644d53c4ffbaad
unknown <Kysluss@aol.com>
2018-09-02 13:39:02 -0400

    Sync'd installed modules

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -5,6 +5,7 @@ import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
 
@@ -13,14 +14,19 @@ class Orders extends Component {
     }
     
     render() {
+        let orders = <Spinner />
+
+        if(!this.props.loading) {
+            orders = this.props.orders.map(order => (
+                <Order 
+                    key={order.id} 
+                    ingredients={order.ingredients} 
+                    price={order.price} />
+            ));
+        }
         return (
             <div>
-                {this.props.orders.map(order => (
-                    <Order 
-                        key={order.id} 
-                        ingredients={order.ingredients} 
-                        price={order.price} />
-                ))}
+                {orders}
             </div>
         );
     }
