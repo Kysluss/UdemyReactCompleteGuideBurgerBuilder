1d779ccda13e857c5621c1f36de4072ab362b7a7
unknown <Kysluss@aol.com>
2018-09-05 20:14:05 -0400

    Retrieved existing token on app load

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -26,6 +26,7 @@ export const authFail = (error) => {
 export const logout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     };
@@ -59,7 +60,9 @@ export const auth = (email, password, isSignup) => {
                 // Create a new Date object from that timestamp
                 const expirationDate = new Date(new Date().getTime() + (response.data.expiresIn * 1000));
                 localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate)
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
+
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
@@ -73,5 +76,26 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH, 
         path: path
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if(!token) {
+            dispatch(logout());
+        }
+        else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+
+            if(expirationDate > new Date()) {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+            }
+            else {
+                dispatch(logout());
+            }
+        }
     }
 }
