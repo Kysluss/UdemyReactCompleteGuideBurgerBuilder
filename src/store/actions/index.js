391737e5104101c77efcc8ae40cefe80f3d21d83
unknown <Kysluss@aol.com>
2019-02-10 17:30:05 -0500

    Side effects moved to sagas

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,18 +1,31 @@
 export {
     addIngredient, 
     removeIngredient, 
-    initIngredients 
+    initIngredients, 
+    setIngredients, 
+    fetchIngredientsFailed 
 } from './burgerBuilder';
 
 export {
     purchaseBurger, 
     purchaseInit, 
-    fetchOrders
+    fetchOrders, 
+    purchaseBurgerStart, 
+    purchaseBurgerSuccess, 
+    purchaseBurgerFail, 
+    fetchOrdersStart, 
+    fetchOrdersSuccess, 
+    fetchOrdersFail 
 } from './order';
 
 export {
     auth, 
     logout, 
     setAuthRedirectPath, 
-    authCheckState
+    authCheckState, 
+    logoutSuccess, 
+    authStart, 
+    authSuccess, 
+    authFail, 
+    checkAuthTimeout
 } from './auth';
